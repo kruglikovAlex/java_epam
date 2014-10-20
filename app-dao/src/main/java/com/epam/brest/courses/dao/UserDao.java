e8d7bfcfc3122223f9f4fab8084c914e0c84a48f
kruglikovAlex <kruglikov_alex@tut.by>
2014-10-21 01:00:47 +0300

    test git @ idea @ UserTest.java

---
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.epam.brest.courses.dao;
+import com.epam.brest.courses.domain.User;
+import java.util.List;
+
+/**
+ * Created by irina on 20.10.14.
+ */
+public interface UserDao {
+    public void addUser(User user);
+    public List<User> getUser();
+    public void removeUser(long userId);
+}
---
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -0,0 +1,26 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.domain.User;
+import java.util.List;
+
+/**
+ * Created by irina on 20.10.14.l
+ */
+public class UserDaoImpl implements com.epam.brest.courses.dao.UserDao {
+
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public List<User> getUser() {
+        return null;
+    }
+
+    @Override
+    public void removeUser(long userId) {
+
+    }
+}
