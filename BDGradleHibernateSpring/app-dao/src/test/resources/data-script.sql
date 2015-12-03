89157e9986611bc1747f2ec61e97f0ef8e2ea771
kruglikovAlex <kruglikov_alex@tut.by>
2015-12-04 02:14:04 +0300

    refactoring

--- BDGradleHibernateSpring/app-dao/src/test/resources/data-script.sql
+++ BDGradleHibernateSpring/app-dao/src/test/resources/data-script.sql
@@ -1,7 +1,8 @@
 insert into BANKDEPOSIT (depositId, depositName, depositMinTerm, depositMinAmount, depositCurrency, depositInterestRate, depositAddConditions) values (default,'depositName0',12,100,'usd',4,'condition0');
-insert into BANKDEPOSIT (depositId, depositName, depositMinTerm, depositMinAmount, depositCurrency, depositInterestRate, depositAddConditions) values (default,'depositName1',13,200,'usd',5,'condition1');
+insert into BANKDEPOSIT (depositId, depositName, depositMinTerm, depositMinAmount, depositCurrency, depositInterestRate, depositAddConditions) values (default,'depositName1',13,200,'eur',5,'condition1');
 insert into BANKDEPOSIT (depositId, depositName, depositMinTerm, depositMinAmount, depositCurrency, depositInterestRate, depositAddConditions) values (default,'depositName2',14,300,'usd',6,'condition2');
 insert into BANKDEPOSIT (depositId, depositName, depositMinTerm, depositMinAmount, depositCurrency, depositInterestRate, depositAddConditions) values (default,'depositName3',15,400,'usd',7,'condition3');
+insert into BANKDEPOSIT (depositId, depositName, depositMinTerm, depositMinAmount, depositCurrency, depositInterestRate, depositAddConditions) values (default,'depositName4',16,500,'grb',8,'condition4');
 
 insert into BANKDEPOSITOR (depositorId,depositorName,depositorDateDeposit,depositorAmountDeposit,depositorAmountPlusDeposit,depositorAmountMinusDeposit,depositorDateReturnDeposit,depositorMarkReturnDeposit) values (default,'depositorName1','2014-12-01',1001,20,20,'2014-12-02',0);
 insert into BANKDEPOSITOR (depositorId,depositorName,depositorDateDeposit,depositorAmountDeposit,depositorAmountPlusDeposit,depositorAmountMinusDeposit,depositorDateReturnDeposit,depositorMarkReturnDeposit) values (default,'depositorName6','2014-12-06',999,60,60,'2014-12-07',0);
